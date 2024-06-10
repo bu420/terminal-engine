ea49346837614d98bc5b44c4225f09b592ae3828
Comlud <ludvigss@outlook.com>
2024-06-10 20:39:53 +0200

    Half blocks

--- include/te/rast.h
+++ include/te/rast.h
@@ -2,71 +2,81 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <assert.h>
 
 typedef struct {
     uint8_t r, g, b;
 } rgb;
 
 typedef struct {
+    vec p, tc, n;
+} vertex;
+
+#define CHAR_INFO_DEFAULT_FG 1 << 0
+#define CHAR_INFO_DEFAULT_BG 1 << 1
+
+// Each char_info essentially contains 2 pixels since characters in the terminal are twice as high as wide.
+typedef struct {
     uint32_t ch;
+    uint8_t flags;
     rgb fg, bg;
 } char_info;
 
-typedef enum {
-    RASTER_TO_Z_BUF_AND_CHAR_BUF,
-    RASTER_TO_Z_BUF,
-    RASTER_TO_CHAR_BUF
-} raster_target;
+typedef struct {
+    char_info * char_buf;
+    float * z_buf;
+} framebuf;
+
+typedef char_info (* shader_callback)(vertex v, char_info c, bool overwriting);
 
 float edge(vec a, vec b, vec c) {
     return (c[0] - a[0]) * (b[1] - a[1]) - (c[1] - a[1]) * (b[0] - a[0]);
 }
 
-void triangle(raster_target target, float z_buf[], char_info char_buf[],
-    vec v0, vec v1, vec v2, 
-    bool has_tex_coords, vec tc0, vec tc1, vec tc2,
-    bool has_normals, vec n0, vec n1, vec n2,
-    char_info (* shader_callback)(vec tc, vec n)) {
-    v0[0] /= v0[2]; v0[1] /= v0[2];
-    v1[0] /= v1[2]; v1[1] /= v1[2];
-    v2[0] /= v2[2]; v2[1] /= v2[2];
+void triangle(framebuf fb, vertex v0, vertex v1, vertex v2, shader_callback shader) {
+    v0.p[0] /= v0.p[2]; v0.p[1] /= v0.p[2];
+    v1.p[0] /= v1.p[2]; v1.p[1] /= v1.p[2];
+    v2.p[0] /= v2.p[2]; v2.p[1] /= v2.p[2];
     
-    v0[0] = (1 + v0[0]) * 0.5 * WIDTH; v0[1] = (1 + v0[1]) * 0.5 * HEIGHT;
-    v1[0] = (1 + v1[0]) * 0.5 * WIDTH; v1[1] = (1 + v1[1]) * 0.5 * HEIGHT;
-    v2[0] = (1 + v2[0]) * 0.5 * WIDTH; v2[1] = (1 + v2[1]) * 0.5 * HEIGHT;
+    v0.p[0] = (1 + v0.p[0]) * 0.5 * WIDTH; v0.p[1] = (1 + v0.p[1]) * 0.5 * HEIGHT;
+    v1.p[0] = (1 + v1.p[0]) * 0.5 * WIDTH; v1.p[1] = (1 + v1.p[1]) * 0.5 * HEIGHT;
+    v2.p[0] = (1 + v2.p[0]) * 0.5 * WIDTH; v2.p[1] = (1 + v2.p[1]) * 0.5 * HEIGHT;
 
-    tc0[0] /= v0[2]; tc0[1] /= v0[2]; 
-    tc1[0] /= v1[2]; tc1[1] /= v1[2]; 
-    tc2[0] /= v2[2]; tc2[1] /= v2[2]; 
+    v0.tc[0] /= v0.p[2]; v0.tc[1] /= v0.p[2]; 
+    v1.tc[0] /= v1.p[2]; v1.tc[1] /= v1.p[2]; 
+    v2.tc[0] /= v2.p[2]; v2.tc[1] /= v2.p[2]; 
 
-    v0[2] = 1 / v0[2]; 
-    v1[2] = 1 / v1[2]; 
-    v2[2] = 1 / v2[2]; 
+    v0.p[2] = 1 / v0.p[2]; 
+    v1.p[2] = 1 / v1.p[2]; 
+    v2.p[2] = 1 / v2.p[2]; 
 
-    float area = edge(v0, v1, v2);
+    float area = edge(v0.p, v1.p, v2.p);
 
-    for (int j = 0; j < HEIGHT; ++j) {
-        for (int i = 0; i < WIDTH; ++i) {
-            vec p = {i + 0.5f, j + 0.5f};
+    for (int y = 0; y < HEIGHT; ++y) {
+        for (int x = 0; x < WIDTH; ++x) {
+            vec p = {x + 0.5f, y + 0.5f};
 
-            float w0 = edge(v1, v2, p);
-            float w1 = edge(v2, v0, p);
-            float w2 = edge(v0, v1, p);
+            float w0 = edge(v1.p, v2.p, p);
+            float w1 = edge(v2.p, v0.p, p);
+            float w2 = edge(v0.p, v1.p, p);
 
             if (w0 >= 0 && w1 >= 0 && w2 >= 0) {
                 w0 /= area; 
                 w1 /= area; 
                 w2 /= area;
 
-                float z = 1 / (w0 * v0[2] + w1 * v1[2] + w2 * v2[2]);
+                float z = 1 / (w0 * v0.p[2] + w1 * v1.p[2] + w2 * v2.p[2]);
 
-                if (z > z_buf[j * WIDTH + i]) {
-                    z_buf[j * WIDTH + i] = z;
+                if (z > fb.z_buf[y * WIDTH + x]) {
+                    fb.z_buf[y * WIDTH + x] = z;
 
-                    vec tc = {(w0 * tc0[0] + w1 * tc1[0] + w2 * tc2[0]) * z,
-                        (w0 * tc0[1] + w1 * tc1[1] + w2 * tc2[1]) * z};
-
-                    char_buf[j * WIDTH + i] = shader_callback(tc, NULL);
+                    vertex v;
+                    for (int i = 0; i < 3; i++) {
+                        v.p[i] = v0.p[i] * w0 + v1.p[i] * w1 + v2.p[i] * w2;
+                        v.tc[i] = z * (v0.tc[i] * w0 + v1.tc[i] * w1 + v2.tc[i] * w2);
+                    }
+                    
+                    fb.char_buf[(y / 2) * WIDTH + x] = shader(v, fb.char_buf[(y / 2) * WIDTH + x], y % 2 != 0);
                 }
             }
         }
--- src/main.c
+++ src/main.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
 #define WIDTH 64
-#define HEIGHT 32
+#define HEIGHT 64
+#define HALF_HEIGHT (HEIGHT / 2)
 
 #include "te/linalg.h"
 #include "te/rast.h"
 #include "assets"
 
 #define CHAR_BLOCK 0x8896e2
-#define CHAR_HALF_BLOCK_BOTTOM 0x8496e2
 #define CHAR_HALF_BLOCK_TOP 0x8096e2
+#define CHAR_HALF_BLOCK_BOTTOM 0x8496e2
 #define CHAR_DOTTED_HIGH_DENSITY 0x9396e2
 #define CHAR_DOTTED_MEDIUM_DENSITY 0x9296e2
 #define CHAR_DOTTED_LOW_DENSITY 0x9196e2
 
-float z_buf[WIDTH * HEIGHT];
-char_info char_buf[WIDTH * HEIGHT];
-
 int char_info_to_str(char_info info, char out[], int max) {
+    if (info.flags & CHAR_INFO_DEFAULT_FG && info.flags & CHAR_INFO_DEFAULT_BG) {
+        memcpy(out, "\x1b[0m ", 5);
+        return 5;
+    }
+
     char str[5];
     str[0] = (info.ch >> 0) & 0xFF;
     str[1] = (info.ch >> 8) & 0xFF;
@@ -26,52 +30,79 @@ int char_info_to_str(char_info info, char out[], int max) {
     str[3] = (info.ch >> 24) & 0xFF;
     str[4] = '\0';
 
-    int len = snprintf(out, max - 1, "\x1b[38;2;%i;%i;%im\x1b[48;2;%i;%i;%im%s", 
-        info.fg.r, info.fg.g, info.fg.b, 
-        info.bg.r, info.bg.g, info.bg.b,
-        str);
+    int len = 0;
+
+    if (info.flags & CHAR_INFO_DEFAULT_FG) {
+        len = snprintf(out, max, "\x1b[0m\x1b[48;2;%i;%i;%im%s", 
+            info.bg.r, info.bg.g, info.bg.b,
+            str);
+    }
+    else if (info.flags & CHAR_INFO_DEFAULT_BG) {
+        len = snprintf(out, max, "\x1b[0m\x1b[38;2;%i;%i;%im%s", 
+            info.fg.r, info.fg.g, info.fg.b, 
+            str);
+    }
+    else {
+        len = snprintf(out, max, "\x1b[38;2;%i;%i;%im\x1b[48;2;%i;%i;%im%s", 
+            info.fg.r, info.fg.g, info.fg.b, 
+            info.bg.r, info.bg.g, info.bg.b,
+            str);
+    }
     
     return len;
 }
 
-void clear() {
-    // Restore cursor position.
-    printf("\033[u");
-
-    memset(z_buf, 0, WIDTH * HEIGHT * sizeof(float));
-
-    char_info info = {.ch = ' ', .fg = {0, 0, 0}, .bg = {0, 0, 0}};
-    for (int i = 0; i < WIDTH * HEIGHT; i++) {
-        char_buf[i] = info;
-    }
-}
-
-char_info shader(vec tc, vec n) {
+char_info shader(vertex v, char_info c, bool overwriting) {
     float M = 2.0f;
-    float pattern = (fmod(tc[0] * M, 1.0) > 0.5) ^ (fmod(tc[1] * M, 1.0) < 0.5);
+    float pattern = (fmod(v.tc[0] * M, 1.0) > 0.5) ^ (fmod(v.tc[1] * M, 1.0) < 0.5);
+    rgb color = pattern ? (rgb){200, 180, 80} : (rgb){70, 160, 180};
 
-    char_info info;
-    info.ch = 0x8896e2;
-    if (pattern)
-        info.fg = (rgb){200, 180, 80};
-    else 
-        info.fg = (rgb){70, 160, 180};
-    info.bg = (rgb){0, 0, 0};
-    return info;
+    if (!overwriting) {
+        c.flags &= ~CHAR_INFO_DEFAULT_FG;
+        c.ch = CHAR_HALF_BLOCK_TOP;
+        c.fg = color;
+    }
+    else {
+        if (c.flags & CHAR_INFO_DEFAULT_FG) {
+            c.flags &= ~CHAR_INFO_DEFAULT_FG;
+            c.ch = CHAR_HALF_BLOCK_BOTTOM;
+            c.fg = color;
+        }
+        else {
+            c.flags &= ~CHAR_INFO_DEFAULT_BG;
+            c.bg = color;
+        }
+    }
+
+    return c;
 }
 
 int main() {
     // Make cursor invisible.
-    printf("\x1b[?25l");
+    puts("\x1b[?25l");
 
     // Save cursor position.
-    printf("\033[s");
+    puts("\x1b[s");
 
     mat proj, view;
-    perspective(proj, HEIGHT / (float)WIDTH * 2.0f, 70.0f, 0.0001f, 1000.0f);
+    perspective(proj, HEIGHT / (float)WIDTH, 70.0f, 0.0001f, 1000.0f);
     look_at(view, (vec){-2.3f, 2.3f, -2.3f}, (vec){0.0f, 0.0f, 0.0f}, (vec){0.0f, -1.0f, 0.0f});
 
     while (1) {
+        // Restore cursor position.
+        puts("\x1b[u");
+
+        float z_buf[WIDTH * HEIGHT] = {0};
+
+        char_info char_buf[WIDTH * HALF_HEIGHT];
+
+        char_info info;
+        info.flags = CHAR_INFO_DEFAULT_FG | CHAR_INFO_DEFAULT_BG;
+
+        for (int i = 0; i < WIDTH * HALF_HEIGHT; i++) {
+            char_buf[i] = info;
+        }
+
         vec vertex_buf[36];
         vec tex_coord_buf[36];
         for (int i = 0; i < 36; i++) {
@@ -80,7 +111,6 @@ int main() {
             memcpy(tex_coord_buf[i], cube_tex_coords[cube_tex_coords_indices[i % 6]], 2 * sizeof(float));
         }
 
-        clear();
         clock_t uptime = clock() / (CLOCKS_PER_SEC / 1000);
         mat model = IDENTITY, vp, mvp;
         rotate_y(model, (float)(uptime * M_PI / 1000));
@@ -88,33 +118,33 @@ int main() {
         mat_mul_mat(view, proj, vp);
         mat_mul_mat(model, vp, mvp);
         for (int i = 0; i < 12; i++) {
-            vec tri[3];
-            for (int j = 0; j < 3; j++)
-                vec_mul_mat(vertex_buf[i * 3 + j], mvp, tri[j]);
-            triangle(RASTER_TO_Z_BUF_AND_CHAR_BUF, z_buf, char_buf,
-                tri[0], tri[1], tri[2], 
-                true, tex_coord_buf[i * 3 + 0], tex_coord_buf[i * 3 + 1], tex_coord_buf[i * 3 + 2], 
-                false, NULL, NULL, NULL, 
-                shader);
+            vertex tri[3];
+            for (int j = 0; j < 3; j++) {
+                vec_mul_mat(vertex_buf[i * 3 + j], mvp, tri[j].p);
+                memcpy(tri[j].tc, tex_coord_buf[i * 3 + j], sizeof(vec));
+            }
+            triangle((framebuf){.z_buf = z_buf, .char_buf = char_buf}, tri[0], tri[1], tri[2], shader);
         }
 
-        char screen[WIDTH * HEIGHT * 40 + 200];
+#define MAX_STR_SIZE 45
+
+        char screen[WIDTH * HALF_HEIGHT * MAX_STR_SIZE + HALF_HEIGHT + 1];
         int i = 0;
 
-        for (int y = 0; y < HEIGHT; y++) {
+        for (int y = 0; y < HALF_HEIGHT; y++) {
             for (int x = 0; x < WIDTH; x++) {
-                char str[40];
-                int len = char_info_to_str(char_buf[y * WIDTH + x], str, 40);
-                for (int j = 0; j < len; j++) {
-                    screen[i++] = str[j];
-                }
+                char str[MAX_STR_SIZE];
+                int len = char_info_to_str(char_buf[y * WIDTH + x], str, MAX_STR_SIZE);
+                memcpy(screen + i, str, len);
+                i += len;
             }
             screen[i++] = '\n';
         }
         screen[i++] = '\0';
+
         puts(screen);
     }
 
     // Make cursor visible.
-    printf("\x1b[?25h");
+    puts("\x1b[?25h");
 }
